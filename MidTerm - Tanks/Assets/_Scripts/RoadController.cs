c98fbc134b67c8a50ca130d08bec3cb753b005af
padmachak1974 <padmachak1974@gmail.com>
2016-03-02 19:00:00 -0500

    first commit

---
+++ MidTerm - Tanks/Assets/_Scripts/RoadController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoadController : MonoBehaviour {
+	// PUBLIC INSTANCE VARIABLES
+	public float speed=3f;
+
+	//PRIVATE INSTANCE VARIABLES
+	private Transform _transform;
+	private Vector2 _currentPosition;
+
+	// Use this for initialization
+	void Start () {
+		// Make a reference with the Transform Component
+		this._transform = gameObject.GetComponent<Transform> ();
+		this._Reset ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 Vector2 currentPosition = gameObject.GetComponent<Transform> ().position;
+		currentPosition.y -= speed;
+		gameObject.GetComponent<Transform> ().position = currentPosition;
+
+ 
+		if (currentPosition.y <= -480) {
+			this._Reset();
+		}
+	}
+
+	private void _Reset() {
+		Vector2 resetPosition = new Vector2 (0.0f, 480f);
+		gameObject.GetComponent<Transform> ().position = resetPosition;
+	}
+}
